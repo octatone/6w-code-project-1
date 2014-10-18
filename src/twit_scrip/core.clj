d7d060bdb8975cc3a79e98794b0e345fdb18499d
Raymond May Jr <raymondmay@gmail.com>
2014-10-18 17:54:08 +0200

    cleanup

--- src/twit_scrip/core.clj
+++ src/twit_scrip/core.clj
@@ -7,61 +7,43 @@
 (defn string-to-base64-string [original]
   (String. (b64/encode (.getBytes original)) "UTF-8"))
 
-(def app-consumer-key
-  (System/getenv "KEY"))
-(def app-consumer-secret
-  (System/getenv "SECRET"))
+(def app-consumer-key (System/getenv "KEY"))
+(def app-consumer-secret (System/getenv "SECRET"))
 
-(def token-auth
-  (str "Basic "
-    (string-to-base64-string
-      (str app-consumer-key ":" app-consumer-secret))))
+(def token-auth (str "Basic "
+    (string-to-base64-string (str app-consumer-key ":" app-consumer-secret))))
 (def token-url "https://api.twitter.com/oauth2/token")
 (def token-content-type "application/x-www-form-urlencoded;charset=UTF-8")
 (def token-data "grant_type=client_credentials")
 
 (def search-base "https://api.twitter.com/1.1/search/tweets.json?q=")
 (def search-query "6wunderkinder")
-(def search-url
-  (str search-base search-query "&result_type=recent&count=100"))
+(def search-url (str search-base search-query "&result_type=recent&count=100"))
 
-(defn get-token
-  "GET A TOKEN"
-  []
-  (
-    let [
+(defn get-token []
+  (let [
       options {
         :body token-data
         :headers {
           :authorization token-auth,
           :content-type token-content-type
-        }
-      }
+        }}
       result (client/post token-url options)
-      body  (:body result)
-      body-json (json/read-str body :key-fn keyword)]
+      body-json (json/read-str (:body result) :key-fn keyword)]
     (:access_token body-json)))
 
-(defn get-6w-tweets
-  "GET 6W TWEETS"
-  []
-  (
-    let [
+(defn get-6w-tweets []
+  (let [
       options {
         :headers {
           :authorization (str "Bearer " (get-token))
-        }
-      }
+        }}
       result (client/get search-url options)
-      body (:body result)
-      body-json (json/read-str body :key-fn keyword)]
+      body-json (json/read-str (:body result) :key-fn keyword)]
     (:statuses body-json)))
 
 (defn -main
-  "PRINT THEM"
   [& args]
-  (
-    let [
-      statuses (get-6w-tweets)]
+  (let [statuses (get-6w-tweets)]
     (doseq [status statuses]
       (println (:text status)))))
