8f13fd777d861777e98ae97a14d99dc776d25d8b
Raymond May Jr <raymondmay@gmail.com>
2014-10-18 15:48:03 +0200

    I DEED IT YEY

---
+++ README.md
@@ -0,0 +1,22 @@
+# twit-scrip
+
+I GETS 6W TWEETS
+
+## Install Deps
+
+```
+lein install
+```
+
+## Run
+
+```
+KEY=your-twitter-app-consumer-key SECRET=your-twitter-app-consumer-secret lein run
+```
+
+## License
+
+Copyright © 2014 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject twit-scrip "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+                  [org.clojure/clojure "1.5.1"]
+                  [environ "1.0.0"]
+                  [clj-http "1.0.0"]
+                  [org.clojure/data.codec "0.1.0"]
+                  [org.clojure/data.json "0.2.5"]
+                ]
+  :main ^:skip-aot twit-scrip.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
